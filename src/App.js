4b2641853bade15ddbdafe9d7fdf955c8904e03d
esy <co6747@naver.com>
2021-09-13 17:14:07 +0900

    # react hooks test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 
 const App = () =>{
   const [value,setCount]=useState(0);
+  const [email,setEmail]=useState("");
+  const updateEmail = e =>{
+    const {
+      target:{value}
+    } = e;
+    setEmail(value);
+  };
   return(
     <>
     {value}
     <button onClick={()=> setCount(value+1)}>Plus</button>
+    <button onClick={()=>setCount(value-1)}>Minus</button>
+    <input placeholder="Email" value={email} onchange={updateEmail}/>
     </>
   );
 };
