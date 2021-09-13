c7afecd2ef7c6547077a9159609d56cfe2b63043
esy <co6747@naver.com>
2021-09-13 16:44:09 +0900

    # react hooks test1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React,{Component,useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>null</h1>
-    </div>
+const App = () =>{
+  const [value,setCount]=useState(0);
+  return(
+    <>
+    {value}
+    <button onClick={()=> setCount(value+1)}>Plus</button>
+    </>
   );
-}
+};
 
 export default App;
