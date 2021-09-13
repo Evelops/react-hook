21a85aab2a27cfc6bb612e86a7f07c2a792ca4da
esy <co6747@naver.com>
2021-09-13 18:25:09 +0900

    #React hooks vs Class Component

--- src/App.js
+++ src/App.js
@@ -13,5 +13,36 @@ const App = () =>{
     </>
   );
 };
+/*
+class AppOrigin extends React.Component{
+  state = {
+    value:1
+  };
 
+  render(){
+    const{value}=this.state;
+    return(
+      <div>
+      <button onClick={this.Plus}>Plus</button>
+      <button onClick={this.Minus}>Minus</button>
+      </div>
+    );
+  }
+
+  Plus = () =>{
+    this.Setstate(state=>{
+      return {
+        value:state.value+1
+      };
+    });
+  };
+  Minus = () => {
+    this.Setstate(state=>{
+      return{
+        value:state.value-1
+      };
+    });
+  };
+}*/ // react hooks를 사용했을 땨와, react 에서 class componet를 사용했을 때의 차이, 
+// react hooks를 사용하면 코드가 엄청나게 간결해지고 ,class 개념이 아닌 함수적 프로그래밍을 할 수 있어 간결, 유지보수도 쉬움. 
 export default App;
